38bfb27d084b39dd5640eb5321e7f143c0060a70
kalia <kaliarovich93@gmail.com>
2023-07-30 23:58:14 +0300

    feat: add header

--- help-to-pets/src/App.tsx
+++ help-to-pets/src/App.tsx
@@ -1,20 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./common/components/Header/Header";
 
-type ClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => void;
 
 function App() {
-    const handleClick: ClickHandler = (event) => {
-        console.log(event);
-        // Здесь можно добавить дополнительную логику обработки события
-    };
 
   return (
-  <div>
-      <h1>Привет Андрей!</h1>
-      <button onClick={handleClick}>Жми сюда!</button>
+  <div className={'App'}>
+      <Header/>
   </div>
   )
 }
---
+++ help-to-pets/src/common/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from './Header.module.scss';
+import logo from "../../assets/Logo/logo.png"
+
+const Header = () => {
+    return (
+        <div className={classes.header}>
+            <div className={classes.logo}>
+                <img src={logo} alt="logo"/>
+            </div>
+            <div className={classes.header__menu}>
+                <div className={classes.menu__item}>Início</div>
+                <div className={classes.menu__item}>Centrais</div>
+                <div className={classes.menu__item}>Perdidos</div>
+                <div className={classes.menu__item}>Achados</div>
+                <div className={classes.menu__item}>Adote</div>
+            </div>
+            <button className={classes.header__button}>Cadastre-se</button>
+        </div>
+    );
+};
+
+export default Header;
